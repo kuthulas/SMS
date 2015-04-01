2f5e34e907b921699ef9e8a0e73fc51aba05b591
Kumaran Thulasiraman <kumaranraman@gmail.com>
2015-04-01 18:44:43 -0500

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Admin.create(username:'admin', email:'kumaranraman@gmail.com', password:'admin123')
+User.create(username:'staff', email:'kuthulas@tamu.edu', password:'staff123')
+Event.create(name:'Event', year:'2015', term:'Spring', date:'2015-08-04', location:'SCM', time:'5:00 PM')
+Event.create(name:'Presentation', year:'2015', term:'Spring', date:'2015-04-01', location:'Reed', time:'1:00 PM')
+Event.create(name:'ECEN DS 2', year:'2015', term:'Winter', date:'2015-04-22', location:'Blocker', time:'3:00 PM')
+Event.create(name:'Showcase', year:'2015', term:'Winter', date:'2015-07-16', location:'Zachry', time:'2:00 PM')
+Event.create(name:'CIVC DS 4', year:'2015', term:'Fall', date:'2015-06-18', location:'ETB', time:'12:30 PM')
+Event.create(name:'New', year:'2015', term:'Fall', date:'2015-05-04', location:'HECC', time:'9:30 AM')
