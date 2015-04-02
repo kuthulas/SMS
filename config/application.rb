8a9092d577b98b1aa2e485864f099758a188d17d
Kumaran Thulasiraman <kumaranraman@gmail.com>
2015-04-02 17:23:19 -0500

    Timezone localized

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module Railbreaker
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.time_zone = 'Central Time (US & Canada)'
+    config.active_record.default_timezone = :local
   end
 end
